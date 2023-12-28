3f8ef361bf4f3893fca6d59821e59d24cf30a9a3
iamrubydevi <ariesabyss@gmail.com>
2023-12-28 17:50:31 +0530

    Update routes to include API version

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :messages
+      end
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
