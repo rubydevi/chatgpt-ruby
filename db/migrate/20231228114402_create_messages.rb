ab1a78ff96ac0960b0167065fa73892f76824bc5
iamrubydevi <ariesabyss@gmail.com>
2023-12-30 11:47:25 +0530

    Refactor migration files

--- db/migrate/20231228114402_create_messages.rb
+++ db/migrate/20231228114402_create_messages.rb
@@ -3,8 +3,8 @@ class CreateMessages < ActiveRecord::Migration[7.1]
     create_table :messages do |t|
       t.text :message
       t.text :reply
-      t.references :conversation, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+      t.references :conversation, null: false, foreign_key: { to_table: :conversations }
 
       t.timestamps
     end
