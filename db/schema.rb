1b23b2ef0bfd6615f99bd09b8fc7e16cdef9908f
iamrubydevi <ariesabyss@gmail.com>
2023-12-30 11:48:05 +0530

    Migrate newly refactored files

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,29 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_28_173507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
-    t.text "content"
+  create_table "conversations", force: :cascade do |t|
     t.bigint "user_id", null: false
+    t.boolean "shared", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_conversations_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "message"
+    t.text "reply"
+    t.bigint "user_id", null: false
+    t.bigint "conversation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "google_id"
+    t.string "github_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -37,5 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_28_173507) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "conversations", "users"
+  add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
 end
